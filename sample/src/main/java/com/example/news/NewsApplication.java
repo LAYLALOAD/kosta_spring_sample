b44125c08de859021828806fe110d23492a6d60d
LAYLALOAD <yjkim7885@knou.ac.kr>
2023-10-27 09:43:57 +0900

    231026

--- sample/src/main/java/com/example/news/NewsApplication.java
+++ sample/src/main/java/com/example/news/NewsApplication.java
@@ -1,14 +1,14 @@
-package com.example.sample;
+package com.example.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplication {
+public class NewsApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SampleApplication.class, args);
+		SpringApplication.run(NewsApplication.class, args);
 	}
 
 }
